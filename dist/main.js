f5cfe324a6314db722beb8b147c2e192073f6754
Tyler Pooschke <tyler.pooschke@pm.me>
2022-02-18 10:53:43 -0600

    continued work on ship tests, basic hit and isSunk functionality

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,23 @@
 function createShips(shipName, shipSize) {
-	const hit = () => console.log("hit");
-	const isSunk = () => console.log("isSuck");
 	return {
 		shipName: shipName,
-		shipSize: shipSize,
-		hit,
-		isSunk,
+		shipSize: new Array(shipSize).fill("O"),
+		hit(hitLocation) {
+			this.shipSize.splice(hitLocation, 1, "X");
+			return this.shipSize;
+		},
+		isSunk() {
+			return this.shipSize.includes("O") ? false : true;
+		},
 	};
 }
-(function initializeShips() {
-	const battleShip = createShips("Battleship", 5);
-	const carrier = createShips("Carrier", 6);
-	const destroyer = createShips("Destroyer", 3);
-	const submarine = createShips("Submarine", 3);
-	const patrolBoat = createShips("Patrol Boat", 2);
-})();
+
+// (function initializeShips() {
+// 	const battleShip = createShips("Battleship", 5);
+// 	const carrier = createShips("Carrier", 6);
+// 	const destroyer = createShips("Destroyer", 3);
+// 	const submarine = createShips("Submarine", 3);
+// 	const patrolBoat = createShips("Patrol Boat", 2);
+// })();
 
 export { createShips };
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,17 +1,53 @@
 import { createShips } from "../dist/main";
 
-describe("Successfully creates a ship", () => {
-	it("Creates ship name", () => {
-		expect(createShips("Battleship", 4).shipName).toBe("Battleship");
+// describe.each([
+// 	{ shipName: "Battleship", shipSize: 4 },
+// 	{
+// 		shipName: "Carrier",
+// 		shipSize: 5,
+// 	},
+// 	{ shipName: "Destroyer", shipSize: 3 },
+// 	{ shipName: "Submarine", shipSize: 3 },
+// 	{ shipName: "Patrol Boat", shipSize: 2 },
+// ])(`Creates $shipName`, ({ shipName, shipSize }) => {
+// 	test(`Return ${shipName}`, () => {
+// 		expect(createShips(shipName, shipSize).shipName).toBe(shipName);
+// 	});
+// 	test(`Return ${shipName} ship size as an array`, () => {
+// 		expect(createShips(shipName, shipSize).shipSize).toEqual(
+// 			new Array(shipSize).fill("O")
+// 		);
+// 	});
+// 	test(`Creates ship's hit function`, () => {
+// 		expect(createShips(shipName, shipSize).hit(0)).toEqual();
+// 	});
+
+// 	test(`Creates ship's isSunk function`, () => {
+// 		expect(createShips(shipName, shipSize).isSunk()).toBeTruthy();
+// 	});
+// });
+
+describe("Successfully creates a ship and ship function operate correctly", () => {
+	const battleship = createShips("Battleship", 4);
+
+	it("Correctly creates ship name", () => {
+		expect(battleship.shipName).toBe("Battleship");
 	});
-	it("Creates ship length", () => {
-		expect(createShips("Battleship", 4).shipSize).toBe(4);
+	it("Correctly creates ship length", () => {
+		expect(battleship.shipSize).toEqual(["O", "O", "O", "O"]);
 	});
-	it("Creates ship's hit function", () => {
-		expect(createShips("Battleship", 4).hit()).toBe();
+	it("Correctly alters this shipSize array to reflect hit on first position", () => {
+		expect(battleship.hit(0)).toEqual(["X", "O", "O", "O"]);
+	});
+	it("Correctly alters this shipSize array to reflect hit on first and fourth position", () => {
+		expect(battleship.hit(3)).toEqual(["X", "O", "O", "X"]);
 	});
 
-	it("Creates ship's isSunk function", () => {
-		expect(createShips("Battleship", 4).isSunk()).toBe();
+	it("Correctly identified that a ship has NOT sank", () => {
+		expect(battleship.isSunk()).toBe(false);
+	});
+	it("Correctly identified that a ship has sank", () => {
+		battleship.shipSize = ["X", "X", "X", "X"];
+		expect(battleship.isSunk()).toBe(true);
 	});
 });
